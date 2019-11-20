c3a9b1abddb53c4449f4cb976c43a183ea999620
Kurt Sansom <kayarre@gmail.com>
2019-11-20 08:19:31 -0800

    attempt to get stdout

--- hist/test_elastix.py
+++ hist/test_elastix.py
@@ -3,9 +3,47 @@ import tifffile as tiff
 import utils
 import numpy as np
 
-fixed = "/media/store/krs/caseFiles/vwi_proc/case01/color_case_1_im_0000.tiff"
+from io import StringIO
+from contextlib import redirect_stdout
+import io
 
-moving = "/media/store/krs/caseFiles/vwi_proc/case01/color_case_1_im_0001.tiff"
+import sys
+
+class Capturing(list):
+    def __enter__(self):
+        self._stdout = sys.stdout
+        sys.stdout = self._stringio = StringIO()
+        return self
+    def __exit__(self, *args):
+        self.extend(self._stringio.getvalue().splitlines())
+        del self._stringio    # free up some memory
+        sys.stdout = self._stdout
+
+
+def getElastixParameters(fileContents, fieldName, type="text"):
+  parameterLine = next(s for s in fileContents if "("+fieldName+" " in s)
+  if type == "text":
+      import shlex # use shlex split (instead of regular split) to preserve spaces in quoted string
+      parameters = shlex.split(parameterLine.strip("()"))
+      parameters.pop(0) # remove fieldName
+  else:
+      parameters = parameterLine.strip("()").split(" ")
+      parameters.pop(0) # remove fieldName
+      if type == "float":
+          parameters = [ float(x) for x in parameters ]
+      elif type == "int":
+          parameters = [ int(x) for x in parameters ]
+  return parameters
+
+
+#fixed = "/media/store/krs/caseFiles/vwi_proc/case01/color_case_1_im_0000.tiff"
+
+#moving = "/media/store/krs/caseFiles/vwi_proc/case01/color_case_1_im_0001.tiff"
+
+
+fixed = "/media/sansomk/510808DF6345C808/caseFiles/vwi_proc/case01/color_case_1_im_0000.tiff"
+
+moving = "/media/sansomk/510808DF6345C808/caseFiles/vwi_proc/case01/color_case_1_im_0001.tiff"
 
 im_f = tiff.imread(fixed, key=6)
 im_t = tiff.imread(moving, key=6)
@@ -39,16 +77,30 @@ print( elastix.GetNumberOfMovingImages())
 rigid = sitk.GetDefaultParameterMap("rigid")
 rigid["DefaultPixelValue"] = ['255']
 rigid["WriteResultImage"] = ['false']
-affine = sitk.GetDefaultParameterMap("affine")
-affine["NumberOfResolutions"] = ['5']
-affine["DefaultPixelValue"] = ['255']
-affine["WriteResultImage"] = ['false']
-sitk.PrintParameterMap(affine)
+#rigid["TransformParameters"]  = ["0.180993", "-1.74626", "8.08803"]
+#rigid["CenterOfRotationPoint"]  = ["257.55", "257.55"]
+output = []
+
+f = io.StringIO()
+with redirect_stdout(f):
+    sitk.PrintParameterMap(rigid)
+s = f.getvalue()
+print("this doesn't work", s)
+#start = Capturing(sitk.PrintParameterMap(rigid))
+#sitk.PrintParameterMap(rigid)
+
+
+quit()
+# affine = sitk.GetDefaultParameterMap("affine")
+# affine["NumberOfResolutions"] = ['5']
+# affine["DefaultPixelValue"] = ['255']
+# affine["WriteResultImage"] = ['false']
+# sitk.PrintParameterMap(affine)
 #elastix.SetParameterMap(rigid)
 #quit()
 parameterMapVector = sitk.VectorOfParameterMap()
 parameterMapVector.append(rigid)
-parameterMapVector.append(affine)
+# parameterMapVector.append(affine)
 
 elastix.SetParameterMap(parameterMapVector)
 
@@ -61,6 +113,11 @@ elastix.Execute()
 
 moving_resampled = elastix.GetResultImage()
 tran_map = elastix.GetTransformParameterMap()
+print("break")
+sitk.PrintParameterMap(tran_map)
+print(dir(tran_map))
+print(tran_map.GetTransformParameter("CenterOfRotationPoint"))
+print(tran_map["TransformParameters"])
 test_image = sitk.Transformix(sitk.VectorIndexSelectionCast(t_sitk, 0), tran_map)
 test_image1 = sitk.Transformix(sitk.VectorIndexSelectionCast(t_sitk, 1), tran_map)
 test_image2 = sitk.Transformix(sitk.VectorIndexSelectionCast(t_sitk, 2), tran_map)
